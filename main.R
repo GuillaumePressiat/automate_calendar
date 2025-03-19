f580cecdd2ec8019ac8edcc640ac7a841a0dfe6e
GuillaumePressiat <guillaume.pressiat@gmail.com>
2025-03-19 11:56:58 +0100

    ajout script de lancement global

---
+++ main.R
@@ -0,0 +1,9 @@
+
+
+jour_debut <- '2015-01-01'
+jour_fin   <- '2025-12-31'
+
+source('scripts/00_clean_up.R')
+source('scripts/01_faire_calendrier.R')
+source('scripts/02_exporter_calendrier_arborescence.R')
+source('scripts/03_reformer_calendrier.R')
